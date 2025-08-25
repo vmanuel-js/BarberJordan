9ee4652bf500ae22eab8ebede232abeb7db52074
Victor Jordan <vmjordansolis@gmail.com>
2025-08-25 18:50:04 -0500

    Cambio en el numero del carrito

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,9 +1,25 @@
 function CartWidget() {
   return (
-    <div style={{ fontSize: "1.2rem", cursor: "pointer", margin: "30px" }}>
+    <div
+      style={{
+        backgroundColor: "#3b3b3b",
+        fontSize: "1.2rem",
+        cursor: "pointer",
+        margin: "30px",
+        padding: "10px",
+        borderRadius: "10px",
+      }}
+    >
       🛒
       <span
-        style={{ fontSize: "1.2rem", fontWeight: "bold", padding: "0px 10px" }}
+        style={{
+          backgroundColor: "#1f1f1fff",
+          color: "#fff",
+          fontSize: "1.1rem",
+          fontWeight: "bold",
+          padding: "0px 10px",
+          borderRadius: "10px",
+        }}
       >
         0
       </span>
